96025036c82387b0d5735c14c03169aacc43e62f
cr1stal-7 <116745222+cr1stal-7@users.noreply.github.com>
2023-01-25 19:07:52 +0700

    Add files via upload

---
+++ Chess/ChessWindow.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Chess
+{
+    partial class Chess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Chess));
+            this.GameState = new System.Windows.Forms.Label();
+            this.Timer = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.WhiteTimer = new System.Windows.Forms.Label();
+            this.TimeButton = new System.Windows.Forms.Button();
+            this.BlackTimer = new System.Windows.Forms.Label();
+            this.State = new System.Windows.Forms.GroupBox();
+            this.Developer = new System.Windows.Forms.Label();
+            this.Undo = new System.Windows.Forms.PictureBox();
+            this.Timer.SuspendLayout();
+            this.State.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Undo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameState
+            // 
+            this.GameState.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.GameState.AutoSize = true;
+            this.GameState.BackColor = System.Drawing.Color.Transparent;
+            this.GameState.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameState.ForeColor = System.Drawing.Color.OliveDrab;
+            this.GameState.Location = new System.Drawing.Point(26, 26);
+            this.GameState.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.GameState.Name = "GameState";
+            this.GameState.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.GameState.Size = new System.Drawing.Size(0, 18);
+            this.GameState.TabIndex = 2;
+            this.GameState.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Timer
+            // 
+            this.Timer.BackColor = System.Drawing.Color.Transparent;
+            this.Timer.Controls.Add(this.label5);
+            this.Timer.Controls.Add(this.label4);
+            this.Timer.Controls.Add(this.WhiteTimer);
+            this.Timer.Controls.Add(this.TimeButton);
+            this.Timer.Controls.Add(this.BlackTimer);
+            this.Timer.Location = new System.Drawing.Point(387, 60);
+            this.Timer.Margin = new System.Windows.Forms.Padding(2);
+            this.Timer.Name = "Timer";
+            this.Timer.Padding = new System.Windows.Forms.Padding(2);
+            this.Timer.Size = new System.Drawing.Size(108, 138);
+            this.Timer.TabIndex = 3;
+            this.Timer.TabStop = false;
+            this.Timer.Text = "Время";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(4, 65);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Белые";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 30);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Черные";
+            // 
+            // WhiteTimer
+            // 
+            this.WhiteTimer.AutoSize = true;
+            this.WhiteTimer.BackColor = System.Drawing.Color.Goldenrod;
+            this.WhiteTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WhiteTimer.Location = new System.Drawing.Point(56, 62);
+            this.WhiteTimer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.WhiteTimer.Name = "WhiteTimer";
+            this.WhiteTimer.Size = new System.Drawing.Size(44, 18);
+            this.WhiteTimer.TabIndex = 2;
+            this.WhiteTimer.Text = "00:00";
+            // 
+            // TimeButton
+            // 
+            this.TimeButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.TimeButton.Location = new System.Drawing.Point(28, 102);
+            this.TimeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.TimeButton.Name = "TimeButton";
+            this.TimeButton.Size = new System.Drawing.Size(42, 19);
+            this.TimeButton.TabIndex = 1;
+            this.TimeButton.Text = "Start";
+            this.TimeButton.UseVisualStyleBackColor = true;
+            // 
+            // BlackTimer
+            // 
+            this.BlackTimer.AutoSize = true;
+            this.BlackTimer.BackColor = System.Drawing.Color.Goldenrod;
+            this.BlackTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BlackTimer.Location = new System.Drawing.Point(56, 27);
+            this.BlackTimer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.BlackTimer.Name = "BlackTimer";
+            this.BlackTimer.Size = new System.Drawing.Size(44, 18);
+            this.BlackTimer.TabIndex = 0;
+            this.BlackTimer.Text = "00:00";
+            // 
+            // State
+            // 
+            this.State.Controls.Add(this.GameState);
+            this.State.Location = new System.Drawing.Point(387, 217);
+            this.State.Margin = new System.Windows.Forms.Padding(2);
+            this.State.Name = "State";
+            this.State.Padding = new System.Windows.Forms.Padding(2);
+            this.State.Size = new System.Drawing.Size(110, 56);
+            this.State.TabIndex = 4;
+            this.State.TabStop = false;
+            this.State.Text = "Состояние";
+            // 
+            // Developer
+            // 
+            this.Developer.AutoSize = true;
+            this.Developer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.Developer.Location = new System.Drawing.Point(426, 358);
+            this.Developer.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Developer.Name = "Developer";
+            this.Developer.Size = new System.Drawing.Size(63, 54);
+            this.Developer.TabIndex = 5;
+            this.Developer.Text = "Пашков\r\nРеймер\r\nПИ-12";
+            // 
+            // Undo
+            // 
+            this.Undo.Image = global::Chess.Properties.Resources.undoArrrow;
+            this.Undo.Location = new System.Drawing.Point(394, 22);
+            this.Undo.Margin = new System.Windows.Forms.Padding(2);
+            this.Undo.Name = "Undo";
+            this.Undo.Size = new System.Drawing.Size(24, 21);
+            this.Undo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Undo.TabIndex = 3;
+            this.Undo.TabStop = false;
+            // 
+            // Chess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 421);
+            this.Controls.Add(this.Undo);
+            this.Controls.Add(this.Developer);
+            this.Controls.Add(this.State);
+            this.Controls.Add(this.Timer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(529, 460);
+            this.MinimumSize = new System.Drawing.Size(488, 460);
+            this.Name = "Chess";
+            this.Text = "Шахматы";
+            this.Load += new System.EventHandler(this.Chess_Load);
+            this.Timer.ResumeLayout(false);
+            this.Timer.PerformLayout();
+            this.State.ResumeLayout(false);
+            this.State.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Undo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.Label GameState;
+        private System.Windows.Forms.GroupBox Timer;
+        private System.Windows.Forms.Label BlackTimer;
+        private System.Windows.Forms.Button TimeButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label WhiteTimer;
+        private System.Windows.Forms.GroupBox State;
+        private System.Windows.Forms.Label Developer;
+        private System.Windows.Forms.PictureBox Undo;
+    }
+}
+
